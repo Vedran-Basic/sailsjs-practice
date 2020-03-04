9ab35dc76bd24045092176103cfa7312aa366504
Vedran-Basic <vedran.basic98@gmail.com>
2020-03-04 16:08:05 +0100

    initial commit

---
+++ api/controllers/post/create.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+
+  friendlyName: 'Create',
+
+
+  description: 'Create post.',
+
+
+  inputs: {
+    /* inputs stavljas ono sto primas iz requesta */
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs/* dodas tu inputs ili description ili exits stogod ti treba i to mozes destrukturirati */) {
+
+    /* probaj print this ili object.keys(this) da vidis sto ima u wrapu ove funkcije*/
+    // All done.
+    return;
+
+  }
+
+
+};
---
+++ api/models/Post.js
@@ -0,0 +1,40 @@
+/**
+ * Post.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    title: {
+      type: 'string',
+      required:true,
+
+    },
+
+    // e.g. 3.26
+    body: {
+      type: 'string',
+      required: false,
+      defaultsTo: 'Empty post - no post body given'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
